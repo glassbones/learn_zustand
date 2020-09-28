091bb7fd645877b179a74cdb531a1e15dc8a11d1
Michael <glassbonesofficial@gmail.com>
2020-09-28 03:53:01 -0500

    got pokemon and input, working on filter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
+import create from "zustand";
 import "./styles.css";
 
+const POKEMON_URL = "https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json"
+
+const useStore = create((set) => ({
+  filter: "",
+  pokemon: [],
+  setFilter: (filter) => set((state) => ({ ...state, filter })),
+  setPokemon: (pokemon) => set((state) => ({ ...state, pokemon })),
+}));
+
+const FilterInput =()=> {
+  const filter = useStore((state) => state.filter)
+  const setFilter = useStore((state) => state.setFilter)
+  return ( <input value={filter} onChange={(e) => setFilter( e.target.value )} /> )
+}
+
 export default function App() {
+  const filter = useStore((state) => state.filter)
+  const pokemon = useStore((state) => state.pokemon)
+  const setPokemon = useStore((state) => state.setPokemon)
+  
+
+  React.useEffect(() => {
+    fetch(POKEMON_URL)
+      .then(resp => resp.json())
+      .then(pokemon => setPokemon(pokemon))
+  })
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <FilterInput />
+      </div>
+      {JSON.stringify(pokemon)}
     </div>
   );
 }
